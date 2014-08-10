dbada7aed655d0fa6c437c7dc003e9e8ef2dcdc1
Niclas Leon Bock <niclasleonbock@gmail.com>
2014-08-10 10:24:18 +0200

    updated about page

--- app/templates/about.php
+++ app/templates/about.php
@@ -9,7 +9,8 @@
     <div class="container">
       <div class="inner">
         <h2>About</h2>
-        <p>WSILT is a service with the purpose of telling you to what you could listen to based on the current (<code>/today</code>) or a given weekday (<code>/on/:day</code>). It uses Spotify to directly link to the according songs.</p>
+        <p>WSILT is a service with the purpose of telling you to what you could listen to based on the current (<code>/today</code>) or a given weekday (<code>/on/:day</code>, where <code>:day</code> needs to be replaced by the weekday, e.g. <code>/on/monday</code>). It uses Spotify to directly link to the according songs.</p>
+        <p>For a full list of all songs you could listen to there is <code>/songs/for/:day</code> (e.g. <code>/songs/for/today</code> or <code>/songs/for/monday</code>).</p>
         <p>You think WSILT is missing a specific song? No problem! You can contribute to the song database! Click <a href="https://github.com/niclasleonbock/wsilt-days">here</a> for more information.</p>
         <p>WSILT is an open source project. You can help improve it by contributing on <a href="https://github.com/niclasleonbock/WhatShouldIListenTo">GitHub</a>.</p>
 
